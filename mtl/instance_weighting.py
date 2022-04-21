c102c82c37c2cad1c991bd0ba8f0a69bb8f26816
Jupinder <jsparmar@stanford.edu>
2022-04-21 01:36:18 +0000

    naive pgd implementation

--- mtl/instance_weighting.py
+++ mtl/instance_weighting.py
@@ -118,36 +118,65 @@ def entropy_maximization_pgd(Y, verbose=False, grouping_name=''):
     where we utilize pgd
     """
 
-    grouping_name = grouping_name + 'entropy_maximization'
+    grouping_name = grouping_name + 'entropy_maximization_pgd'
     file_name = grouping_name + '.npy'
     if os.path.exists(os.path.join('./mtl_suby_w', file_name)):
         w = np.load(os.path.join('./mtl_suby_w', file_name))
         return w, grouping_name
 
 
-    Y = Y.astype(np.float32)
+    Y = Y.astype(np.float32).toarray()
+    Y = tf.convert_to_tensor(Y)
     k, n = Y.shape  # k = number of tasks, n = number of instances
     c = 0.5 * np.ones((k,), dtype=np.float32)
+    c = tf.convert_to_tensor(c)
     print(f"Solving for w over {n} examples. Input array of labels: ({k} tasks, {n} examples).")
 
-    # Solve
-    w = cp.Variable(n, nonneg=True)
-    prob = cp.Problem(
-        cp.Maximize(cp.sum(cp.entr(w))),
-        [
-            Y @ w == c,
-            cp.sum(w) == 1,
-        ]
+    class EntropyMaximization(tfco.ConstrainedMinimizationProblem):
+        def __init__(self, loss_fn, weights, Y , c):
+            self.loss_fn = loss_fn
+            self.weights = weights
+            self.Y = Y
+            self.c = c
+    
+        @property
+        def num_constraints(self):
+            return 5
+    
+        def objective(self):
+            return self.loss_fn()
+    
+        def constraints(self):
+            sum_weights = tf.math.reduce_sum(self.weights)
+            lt_or_eq_one = sum_weights - 1
+            gt_or_eq_one = 1 - sum_weights
+            y_at_w = tf.matmul(self.Y, self.weights)
+            lt_or_eq_c = y_at_w - c
+            gt_or_eq_c = c - y_at_w 
+            constraints = tf.stack([lt_or_eq_one, gt_or_eq_one, lt_or_eq_c, gt_or_eq_c, -self.weights])
+            return constraints
+
+    w = tf.Variable(tf.zeros(n), dtype=tf.float32, name='w')
+
+    def loss_fn():
+        return -1*(-1*tf.reduce_sum(tf.math.multiply(w,tf.math.log(w))))
+
+    problem = EntropyMaximization(loss_fn, w, Y, c)
+
+    optimizer = tfco.LagrangianOptimizer(
+        optimizer=tf.optimizers.Adagrad(learning_rate=0.1),
+        num_constraints=problem.num_constraints
     )
 
-    start = time.time()
-    prob.solve(solver=cp.ECOS, verbose=verbose, qcp=True)
-    print("Caught ECOS error, probably due to unconverged tolerance")
-    end = time.time()
+    var_list = w + problem.trainable_variables + optimizer.trainable_variables()
 
-    print(f"Took {end - start:.2f} seconds to solve the entropy minimization problem.")
-    print(w.value)
-    return w.value, grouping_name
+    for i in range(10000):
+        optimizer.minimize(problem, var_list=var_list)
+        if i % 1000 == 0:
+            print(f'step = {i}')
+            print(f'loss = {loss_fn()}')
+
+    return w.numpy(), grouping_name
 
 
 
@@ -173,7 +202,7 @@ if __name__ == "__main__":
     dataset = CelebA(config, split="train")
     Y = dataset.attr[:, dataset.task_label_indices].T.numpy()  # (7, 162770)
     Y = scipy.sparse.csr_matrix(Y)
-    w, grouping_name = entropy_maximization(Y, verbose=True, grouping_name = grouping_name)
+    w, grouping_name = entropy_maximization_pgd(Y, verbose=True, grouping_name = grouping_name)
     print(f"verify that the weights multiply against the multi-class labels to 1/2: {Y@w}")
     print(f"verify that weights sum to one: {np.sum(w)}")
     print(f'minimum value of weight: {np.amin(w)}')
